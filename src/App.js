a01632e27b368a93d925703b40557dbbebc8e4a3
Klippie <info@johanvanderklift.nl>
2022-11-28 17:02:29 +0100

    Implemented routes

--- src/App.js
+++ src/App.js
@@ -5,15 +5,19 @@ import Home from './pages/home/Home';
 import Appointments from './pages/appointments/Appointments';
 import Cavities from './pages/cavities/Cavities';
 import Navigation from './components/navigation/Navigation';
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Navigation />
-      <Home />
-      <Whitening />
-      <Appointments />
-      <Cavities />
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='tanden-bleken' element={<Whitening/>}/>
+        <Route path='Afspraken' element={<Appointments/>}/>
+        <Route path='Gaatjes' element={<Cavities/>}/>
+      </Routes>
+
     </>
   );
 }
